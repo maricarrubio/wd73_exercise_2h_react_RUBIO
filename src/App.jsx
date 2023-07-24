91d4c956ecec883eae9400af1b158f9ba49b2658
Maricar Rubio <maricarlaquianrubio02@gmail.com>
2023-07-24 21:46:17 +0800

    linked pages to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import ProductList from "./components/ProductList"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   
-
   return (
     <>
-      <h1>Hello World</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<JewelryPage />} />
+        <Route path="/" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+      
     </>
   )
 }
